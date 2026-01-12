5ff47eaa5c70e94c8b82baf99fb91f9445fb565c
THAI MINH CHI <chi.thai@mail.finviet.com.vn>
2026-01-12 08:31:20 +0700

    add service name to logger

--- src/modules/category/presentation/category.controller.ts
+++ src/modules/category/presentation/category.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ServiceName } from 'src/shared/logger/decorators/service-name.decorator';
 import { CreateCategoryDto } from '../application/dtos/create-category.dto';
 import { CreateCategoryUseCase } from '../application/usecases/create-category.usecase';
 import { GetAllCategoryUseCase } from '../application/usecases/get-all-category.usecase';
 
 @Controller('categories')
+@ServiceName('categories-service')
 export class CategoryController {
   constructor(
     private readonly createCategory: CreateCategoryUseCase,
